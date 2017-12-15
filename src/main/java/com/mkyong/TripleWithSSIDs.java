1eb8d150f7df0b2bda9cbaba61588fded8e6f6b7
tgs <tgs@tgs.blue>
2017-12-15 20:22:53 +0800

    Succeed

---
+++ src/main/java/com/mkyong/TripleWithSSIDs.java
@@ -0,0 +1,13 @@
+package com.mkyong;
+
+import com.scir.hypernym.webservice.TripleWithSSID;
+
+import java.util.ArrayList;
+
+public class TripleWithSSIDs {
+    ArrayList<TripleWithSSID> triple_with_ssid_s;
+
+    public TripleWithSSIDs() {
+        this.triple_with_ssid_s = new ArrayList<TripleWithSSID>();
+    }
+}
